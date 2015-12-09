da7ec487364abd2bb9cd9a6fa5c90bd6cef35ac4
unknown <lili10@697-334-Edoras.sf.k12.internal>
2015-12-09 08:31:08 -0800

    Added first name to index.html

---
+++ build-tmp/source/Asteroids2.java
@@ -0,0 +1,242 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Asteroids2 extends PApplet {
+
+//your variable declarations here
+SpaceShip bob = new SpaceShip();
+Asteroid[] ling = new Asteroid[20];
+Star[] li = new Star[400];
+boolean up=false;
+boolean left=false;
+boolean right=false;//your variable declarations here
+public void setup() 
+{
+  size(600,600);
+  background(0);
+  li = new Star[400];
+  for(int i=0; i<li.length;i++)
+  {
+    li[i] = new Star();
+  }
+  ling = new Asteroid[20];
+  for(int i=0; i<ling.length;i++)
+  { 
+    ling[i] = new Asteroid();
+  }
+}
+public void draw()
+{
+  background(0);
+  if(up){bob.accelerate(.1f);}
+  if(left){bob.rotate(-5);}
+  if(right){bob.rotate(5);}
+  for(int i=0;i<li.length;i++)
+    li[i].show();
+  for(int i=0;i<ling.length;i++)
+  {
+    ling[i].move();
+    ling[i].show();
+  }
+  bob.move();
+  bob.show();
+}
+public void keyPressed()
+{  
+      if(key=='w'){up=true;}
+      if(key=='a'){left=true;}
+      if(key=='d'){right=true;}
+}
+public void keyReleased()
+{  
+      if(key=='w'){up=false;}
+      if(key=='a'){left=false;}
+      if(key=='d'){right=false;}
+}
+class Star{
+  private int myX, myY;
+  public Star()
+  {
+    myX = (int)(Math.random()*600);
+    myY = (int)(Math.random()*600);
+  }
+  public void show()
+  {
+    fill(255,255,204);
+    ellipse(myX,myY,1,1);
+  }
+}
+class Asteroid extends Floater
+{
+  public void setX(int x){myCenterX = x;} 
+  public int getX(){return (int)myCenterX;}   
+  public void setY(int y){myCenterY = y;}   
+  public int getY(){return (int)myCenterY;}   
+  public void setDirectionX(double x){myDirectionX=x;}   
+  public double getDirectionX(){return (int)myDirectionX;}   
+  public void setDirectionY(double y){myDirectionY=y;}   
+  public double getDirectionY(){return (int) myDirectionY;}   
+  public void setPointDirection(int degrees){myPointDirection = degrees;}   
+  public double getPointDirection(){return(int)myPointDirection;}
+  private int spin;
+  public Asteroid()
+  {
+      myColor=(205);
+      myCenterX = 300;
+      myCenterY = 300;
+      corners = 4;
+      xCorners = new int[corners];
+      yCorners = new int[corners];
+      xCorners[0] = -8;
+      yCorners[0] = -8;
+      xCorners[1] = 16;
+      yCorners[1] = 0;
+      xCorners[2] = -8;
+      yCorners[2] = 8;
+    setX ((int)0);
+    setY ((int)(Math.random()*600));
+    setDirectionX ((Math.random()*5)-2);
+    setDirectionY ((Math.random()*5)-2);
+    setPointDirection((int)(Math.random()*360));
+    spin = (int)((Math.random()*5)-3);
+  }
+  public void move()
+  {
+    super.move();
+    rotate(spin);
+  }
+}
+class SpaceShip extends Floater  
+{   
+  public SpaceShip(){
+      myColor=(255);
+      myCenterX = 300;
+      myCenterY = 300;
+      corners = 4;
+      xCorners = new int[corners];
+      yCorners = new int[corners];
+      xCorners[0] = -8;
+      yCorners[0] = -8;
+      xCorners[1] = 16;
+      yCorners[1] = 0;
+      xCorners[2] = -8;
+      yCorners[2] = 8;
+    for (int i=0;i<corners;i++)
+    {
+      xCorners[i] *= 2.2f;
+      yCorners[i] *= 2.2f;
+    }
+  }
+  public void setX(int x){myCenterX = x;} 
+  public int getX(){return (int)myCenterX;}   
+  public void setY(int y){myCenterY = y;}   
+  public int getY(){return (int)myCenterY;}   
+  public void setDirectionX(double x){myDirectionX=x;}   
+  public double getDirectionX(){return (int)myDirectionX;}   
+  public void setDirectionY(double y){myDirectionY=y;}   
+  public double getDirectionY(){return (int) myDirectionY;}   
+  public void setPointDirection(int degrees){myPointDirection = degrees;}   
+  public double getPointDirection(){return(int)myPointDirection;}
+  }
+abstract class Floater //Do NOT modify the Floater class! Make changes in the SpaceShip class 
+{   
+  protected int corners;  //the number of corners, a triangular floater has 3   
+  protected int[] xCorners;   
+  protected int[] yCorners;   
+  protected int myColor;   
+  protected double myCenterX, myCenterY; //holds center coordinates   
+  protected double myDirectionX, myDirectionY; //holds x and y coordinates of the vector for direction of travel   
+  protected double myPointDirection; //holds current direction the ship is pointing in degrees    
+  abstract public void setX(int x);  
+  abstract public int getX();   
+  abstract public void setY(int y);   
+  abstract public int getY();   
+  abstract public void setDirectionX(double x);   
+  abstract public double getDirectionX();   
+  abstract public void setDirectionY(double y);   
+  abstract public double getDirectionY();   
+  abstract public void setPointDirection(int degrees);   
+  abstract public double getPointDirection(); 
+
+  //Accelerates the floater in the direction it is pointing (myPointDirection)   
+  public void accelerate (double dAmount)   
+  {          
+    //convert the current direction the floater is pointing to radians    
+    double dRadians =myPointDirection*(Math.PI/180);     
+    //change coordinates of direction of travel    
+    myDirectionX += ((dAmount) * Math.cos(dRadians));    
+    myDirectionY += ((dAmount) * Math.sin(dRadians));       
+  }   
+  public void rotate (int nDegreesOfRotation)   
+  {     
+    //rotates the floater by a given number of degrees    
+    myPointDirection+=nDegreesOfRotation;   
+  }   
+  public void move ()   //move the floater in the current direction of travel
+  {      
+    //change the x and y coordinates by myDirectionX and myDirectionY       
+    myCenterX += myDirectionX;    
+    myCenterY += myDirectionY;     
+
+    //wrap around screen    
+    if(myCenterX >width)
+    {     
+      myCenterX = 0;    
+    }    
+    else if (myCenterX<0)
+    {     
+      myCenterX = width;    
+    }    
+    if(myCenterY >height)
+    {    
+      myCenterY = 0;    
+    }   
+    else if (myCenterY < 0)
+    {     
+      myCenterY = height;    
+    }   
+  }   
+  public void show ()  //Draws the floater at the current position  
+  {             
+    fill(myColor);   
+    stroke(myColor);    
+    //convert degrees to radians for sin and cos         
+    double dRadians = myPointDirection*(Math.PI/180);                 
+    int xRotatedTranslated, yRotatedTranslated;    
+    beginShape();         
+    for(int nI = 0; nI < corners; nI++)    
+    {     
+      //rotate and translate the coordinates of the floater using current direction 
+      xRotatedTranslated = (int)((xCorners[nI]* Math.cos(dRadians)) - (yCorners[nI] * Math.sin(dRadians))+myCenterX);     
+      yRotatedTranslated = (int)((xCorners[nI]* Math.sin(dRadians)) + (yCorners[nI] * Math.cos(dRadians))+myCenterY);      
+      vertex(xRotatedTranslated,yRotatedTranslated);    
+    }   
+    endShape(CLOSE);  
+  }   
+} 
+
+
+
+
+
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Asteroids2" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
